e2823800ba67fc6b93c2e36eeae31d92a692e4dc
KhoiDOO <khoido8899@gmail.com>
2023-05-19 14:22:47 +0700

    update create graph and retain graph for khlars

--- train.py
+++ train.py
@@ -187,7 +187,8 @@ def main_worker(gpu, args):
             
             optimizer.zero_grad()
             loss.backward(
-                retain_graph = True if args.opt == 'khlars' else False
+                retain_graph = True if args.opt == 'khlars' else False,
+                create_graph = True if args.opt == 'khlars' else False
             )
             optimizer.step()
             
