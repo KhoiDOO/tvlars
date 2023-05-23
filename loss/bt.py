d9dc3f290dec5bdee89dcbe6adc733449bc33a69
KhoiDOO <khoido8899@gmail.com>
2023-05-23 09:57:49 +0700

    update lambda factor for bt loss function

--- loss/bt.py
+++ loss/bt.py
@@ -8,8 +8,9 @@ def off_diagonal(x):
     return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
 
 class BTLoss(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, bt_lambd = 0.005, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.bt_lambd = bt_lambd
     
     def forward(self, z1, z2) -> torch.Tensor:
         # empirical cross-correlation matrix
@@ -21,5 +22,5 @@ class BTLoss(nn.Module):
 
         on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
         off_diag = off_diagonal(c).pow_(2).sum()
-        loss = on_diag + self.args.lambd * off_diag
+        loss = on_diag + self.bt_lambd * off_diag
         return loss
