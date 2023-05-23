524574160b68f6309eed2e9a375310731042c659
KhoiDOO <khoido8899@gmail.com>
2023-05-23 14:29:06 +0700

    simplify training

--- self_sl.py
+++ self_sl.py
@@ -3,6 +3,7 @@ import argparse
 import pandas as pd
 import pickle
 from tqdm import tqdm
+from util import *
 
 import torch
 from torch import nn
@@ -20,37 +21,11 @@ from scheduler.base import get_sche
 from scheduler.lars_warmup import adjust_learning_rate
 from metric import accuracy
 
-def folder_setup(args: argparse):
-    runs_dir = os.getcwd() + "/cl_runs"
-    if not os.path.exists(runs_dir):
-        os.mkdir(runs_dir)
-    
-    data_model_dir = runs_dir + f"/{args.ds}_{args.model}"
-    if not os.path.exists(data_model_dir):
-        os.mkdir(data_model_dir)
-    
-    if args.opt in ['adam', 'adamw', 'adagrad', 'rmsprop', 'lars', 'clars', 'khlars', 'lamb']:
-        opt_dir = data_model_dir + f"/{args.opt}"
-    elif args.opt == 'tvlars':
-        opt_dir = data_model_dir + f"/{args.opt}_{args.lmbda}"
-    
-    if not os.path.exists(opt_dir):
-        os.mkdir(opt_dir)
-    
-    return opt_dir
-
 def main(args: argparse):
     
     # Setup folder
     args.log_dir = folder_setup(args=args)
-    check_log_path = args.log_dir + f"/{args.bs}_{args.lr}_{args.sd}.parquet"
-    check_ratio_path = args.log_dir + f"/{args.bs}_{args.lr}_{args.sd}.pickle"
-    filename = "/".join(check_log_path.split("/")[-3:]).replace('parquet', '')
-    if os.path.exists(check_log_path) and os.path.exists(check_ratio_path):
-        print(f"{filename}: Existed -> Skipped")
-        exit(0)
-    else:
-        print(f"{filename}: Non-existed -> Conducted")
+    check_exp_exist(args=args)
     
     # Setup Multi GPU Training
     args.ngpus = torch.cuda.device_count()
